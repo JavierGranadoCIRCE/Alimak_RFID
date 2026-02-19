d399537242167a9ae4f7cb6805f52be85bb01543
Javier Granado <601354@unizar.es>
2026-02-19 10:51:13 +0100

    new brach write

--- RFID_ALIMAK_VGT6/Core/Src/main.c
+++ RFID_ALIMAK_VGT6/Core/Src/main.c
@@ -24,6 +24,7 @@
 /* prueba commit */
 /* prueba commit */
 /* prueba commit */
+/* prueba commit new brach write */
 
 /* ===================== CONFIG ===================== */
 #define DT_BUF_SZ        4096
